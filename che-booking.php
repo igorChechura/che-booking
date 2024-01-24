4458eba7c889914a2ca06633a3be58ec3325c8cd
igorChechura <igorchechura@gmail.com>
2024-01-24 12:56:50 +0300

    register_post_type for rooms

--- che-booking.php
+++ che-booking.php
@@ -5,6 +5,27 @@ Description: Booking plugin.
 Author: Igor Chechura
 Version: 1.0
 */
-if ( !function_exists( 'add_action' ) ) {
+if (!function_exists('add_action')) {
 	exit;
+}
+
+class CheBooking
+{
+	function __construct()
+	{
+		add_action('init', array($this, 'custom_post_type'));
+	}
+
+	function custom_post_type()
+	{
+		register_post_type('room', [
+			'public' => true,
+			'label' => esc_html__('Rooms', 'chebooking'),
+			'supports' => ['title', 'editor', 'thumbnail'],
+		]);
+	}
+}
+
+if (class_exists('CheBooking')) {
+	new CheBooking();
 }
