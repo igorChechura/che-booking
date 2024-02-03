ed0ca3baab10ec9f39aa4d71f951de53c2135926
igorChechura <igorchechura@gmail.com>
2024-02-03 21:57:02 +0300

    Filter select type, html

--- templates/archive-room.php
+++ templates/archive-room.php
@@ -18,9 +18,19 @@ $instance = new CheBooking();
                     <option value=""><?php esc_html_e('Select location', 'chebooking'); ?></option>
 
                     <?php
-                        $instance->get_terms_hierarhical('location');
+                    $instance->get_terms_hierarhical('location');
                     ?>
                 </select>
+
+                <select name="type">
+                    <option value=""><?php esc_html_e('Select type', 'chebooking'); ?></option>
+
+                    <?php
+                    $instance->get_terms_hierarhical('type');
+                    ?>
+                </select>
+
+                <input type="submit" name="submit" value="<?php esc_html_e('Filter', 'chebooking'); ?>">
 
             </form>
         </div>
@@ -31,10 +41,33 @@ $instance = new CheBooking();
                 the_post(); ?>
 
                 <article id="post=<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <?php
+                    if (get_the_post_thumbnail(get_the_ID(), 'large')) {
+                        echo '<div class="image">' . get_the_post_thumbnail(get_the_ID(), 'large') . '</div>';
+                    }
+                    ?>
                     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <?php the_excerpt(); ?>
-                </article>
 
+                    <div class="description">
+                        <?php the_excerpt(); ?>
+                    </div>
+
+                    <?php
+                    $locations = get_the_terms(get_the_ID(), 'location');
+                    if (!empty($locations)) {
+                        foreach ($locations as $location) {
+                            echo '<div class="location">' . esc_html__('Location', 'chebooking') . $location->name . '</div>';
+                        }
+                    }
+
+                    $types = get_the_terms(get_the_ID(), 'type');
+                    if (!empty($types)) {
+                        foreach ($types as $type) {
+                            echo '<div class="type">' . esc_html__('Type', 'chebooking') . $type->name . '</div>';
+                        }
+                    }
+                    ?>
+                </article>
         <?php }
 
             echo paginate_links();
